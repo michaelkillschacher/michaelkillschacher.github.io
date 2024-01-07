0b7350502edd9a0494e7e1723cb6c5747192170f
michaelkillschacher <120222800+michaelkillschacher@users.noreply.github.com>
2024-01-07 22:53:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     var mobileHoverElement = document.getElementById('mobileHoverElement');
 
     // Event Listener für Touchstart und Touchend
     mobileHoverElement.addEventListener('touchstart', function () {
         mobileHoverElement.classList.add('touch');
+        console.log('Touchstart Event');
     });
 
     mobileHoverElement.addEventListener('touchend', function () {
         mobileHoverElement.classList.remove('touch');
+        console.log('Touchend Event');
     });
 });
