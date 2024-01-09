b90b2f76d84fccd2e6bb2133871329eca0e274a6
michaelkillschacher <120222800+michaelkillschacher@users.noreply.github.com>
2024-01-09 12:18:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,60 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileHoverElement.addEventListener('mouseover', toggleTouchClass);
     mobileHoverElement.addEventListener('mouseout', toggleTouchClass);
 });
+
+
+
+
+
+
+
+angular.module('app', [])
+
+.filter('sanitize', function($sce) {
+  return function(htmlCode){
+    return $sce.trustAsHtml(htmlCode);
+  }
+})
+
+.controller('baseController', function() {
+  this.notifications = {
+    'May 21, 2015': [
+      {
+        img: 'https://s3.amazonaws.com/uifaces/faces/twitter/_everaldo/128.jpg',
+        text: '<strong>Mad Jack</strong> added you to the board <a href="javascript:void(0);">The treasure of the three witches</a>',
+        time: '9:40 PM'
+      }
+    ],
+    'May 20, 2015': [
+      {
+        img: 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg',
+        text: '<p><strong>Snuck</strong> commented on <a href="javascript:void(0);">Adventures of the high sees</a></p><p>Can\'t wait to go on this adventure with you, captain. it\'s a honor and a privilege to serve as your first mate..."</p>',
+        time: '4:42 PM'
+      },
+      {
+        img: 'https://s3.amazonaws.com/uifaces/faces/twitter/alexcican/128.jpg',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula posuere ligula at aliquet. Sed ornare, lacus quis hendrerit fringilla, augue quam tincidunt ante, laoreet fringilla elit odio non eros. Sed aliquam augue vitae leo cursus, ut facilisis metus fringilla. Aliquam nisi ipsum, tincidunt a orci ac, fermentum cursus purus. Nunc commodo, odio quis faucibus imperdiet, ante arcu aliquet odio, nec tincidunt sapien tortor quis nisi. Duis rhoncus sapien dolor, sed efficitur orci viverra a. Etiam eget mollis mi. Donec sagittis vulputate enim.',
+        time: '1:22 AM'
+      },
+    ],
+    'April 8, 2015': [
+      {
+        img: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        text: '<p><strong>Angus Dagnabbit</strong> moved <a href="javascript:void(0);">Pirate\'s treasure from</a> <span class="label label-warning">In progress</span> to <span class="label label-success">Done</span></p>',
+        time: '2:09 PM'
+      }
+    ],
+    'April 6, 2015': [
+      {
+        img: 'https://s3.amazonaws.com/uifaces/faces/twitter/alexcican/128.jpg',
+        text: '<p><strong>Flash Dashing</strong> uploaded 4 new screenshots to the board <a href="javascript:void(0);">Steal Mad Jack\'s treasure again</a></p>',
+        time: '4:22 PM'
+      },
+      {
+        img: 'https://s3.amazonaws.com/uifaces/faces/twitter/_everaldo/128.jpg',
+        text: 'Aliquam faucibus mauris sit amet blandit placerat. Maecenas interdum sapien ante, ut sollicitudin ex semper convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam id maximus est. Vestibulum ullamcorper semper ante, quis tristique odio aliquam eget. Sed ut luctus augue. Mauris sollicitudin tellus a malesuada ultricies. Fusce purus est, dapibus quis ex ac, egestas ornare orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at consequat lectus.',
+        time: '12:20 PM'
+      }
+    ]
+  };
+});
